eb3805f09eb0cb4552df47186219dfc1c10c423f
willianchu <willianchu@hotmail.com>
2022-05-12 21:01:57 -0700

    sets first recipes route

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const recipesRoutes = require('./routes/recipes.js');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/recipes', recipesRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello from the server!');
+});
 
-app.listen(port, () => {
-  console.log(`Server app listening on port ${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server App listening on port ${PORT}`)
+});
